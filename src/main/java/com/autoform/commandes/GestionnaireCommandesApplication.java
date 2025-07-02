3ed764abb3170011834d4b3cd92c7effaed9ed85
Nahuel Vainer <nahuel9@hotmail.fr>
2025-07-02 13:54:07 +0200

    Init

---
+++ src/main/java/com/autoform/commandes/GestionnaireCommandesApplication.java
@@ -0,0 +1,13 @@
+package com.autoform.commandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionnaireCommandesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionnaireCommandesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/autoform/commandes/GestionnaireCommandesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoform.commandes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionnaireCommandesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
